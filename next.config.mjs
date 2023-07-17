389f6afa5688eaedd60afac9c8bcb42e75e3d2de
holyaustin <holyaustin@yahoo.com>
2023-07-17 19:12:11 +0100

    Config.ts commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ import './env.mjs'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+},
   images: {
     domains: ['avatars.githubusercontent.com', 'images.unsplash.com', 'cloudflare-ipfs.com'],
   },
